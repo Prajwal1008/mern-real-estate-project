7ecdf80cdc88f574cf5046fff682823dcd832349
Prajwal <prajwalbankar117@gmail.com>
2024-04-20 11:23:03 +0530

    MongoDB Connection Atlas

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+});
+
 const app = express();
+
 const Port = 8000;
+
 app.use(express.json());
+
 app.listen(Port, () => {
     console.log('Server is running on port 8000!!');
     }
